7686a27034d310e50311cef5237e6ef7e7631694
Adam Jakubek <ajakubek@gmail.com>
2011-12-22 03:58:09 +0100

    Added more distutils metadata, fixed building of source package

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from distutils.core import setup, Extension
 
 sources=[ 'src/llist.c',
@@ -6,6 +9,22 @@ sources=[ 'src/llist.c',
           ]
 
 setup(name='llist',
+      description='Linked list data structures for Python',
+      author=u'Adam Jakubek <ajakubek@gmail.com>, ' \
+             u'Rafał Gałczyński <rafal.galczynski@gmail.com>',
       version='0.1',
-      ext_modules=[ Extension('llist', sources) ]
+      url='https://github.com/ajakubek/python-llist',
+      license='MIT',
+      ext_modules=[ Extension('llist', sources) ],
+      classifiers=[
+        'Development Status :: 3 - Alpha'
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License'
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: C',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
       )
