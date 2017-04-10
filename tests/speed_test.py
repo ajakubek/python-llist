d95db50d75ddb9e23d36c1d585f04c9420bffa15
Tim Savannah <kata198@gmail.com>
2017-04-09 22:11:32 -0400

    speed_text: fix columns to always line up and make speed_test executable

--- tests/speed_test.py
+++ tests/speed_test.py
@@ -43,8 +43,7 @@ for container in [deque, dllist, sllist]:
         start = time.time()
         operation(c)
         elapsed = time.time() - start
-        print ( "Completed %s/%s in \t\t%.8f seconds:\t %.1f ops/sec" % (
-            container.__name__,
-            operation.__name__,
-            elapsed,
-            num / elapsed) )
+
+        col1 = "Completed %s/%s in" %(container.__name__, operation.__name__)
+        col2 = "%.8f seconds:    %.1f" %(elapsed, num / elapsed )
+        print ( "%s%s    %s%s ops/s" %(col1, (35 - len(col1)) * ' ', col2, (36 - len(col2)) * ' ') )
