44859204bafe516551db37d332ca9e2d14136a92
alexstennet <alexstennet@berkeley.edu>
2018-06-15 21:21:38 -0700

    Add tests for appendnode for both sllist and dllist

--- tests/llist_test.py
+++ tests/llist_test.py
@@ -285,6 +285,13 @@ class testsllist(unittest.TestCase):
         ll.appendleft(5)
         self.assertTrue([1, 2, 3, 4, 5], list(ll))
 
+    def test_append_node(self):
+        ll = sllist([1, 2, 3, 4])
+        node = sllistnode(5)
+        ll.appendnode(node)
+        self.assertEqual([1, 2, 3, 4, 5], list(ll))
+        self.assertIs(node, ll.last)
+
     def test_pop_left_from_one_elem(self):
         ll = sllist(py23_xrange(0, 100))
         dd = ll.popleft()
@@ -1183,6 +1190,13 @@ class testdllist(unittest.TestCase):
         self.assertEqual(prev.next, new_node)
         self.assertEqual(ll.last, new_node)
         self.assertEqual(ll, ref)
+
+    def test_append_node(self):
+        ll = dllist([1, 2, 3, 4])
+        node = dllistnode(5)
+        ll.appendnode(node)
+        self.assertEqual([1, 2, 3, 4, 5], list(ll))
+        self.assertIs(node, ll.last)
 
     def test_extend(self):
         a_ref = py23_range(0, 1024, 4)
